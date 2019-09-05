3c9cee694c23955810ecb537c4d7fe9a1f5e7d63
vagrant <vagrant@localhost.localdomain>
2019-09-05 16:44:52 +0900

    テスト

---
+++ User.class.php
@@ -0,0 +1,14 @@
+<?php
+
+namespase Dotinstall\Lib;
+
+class User {
+  public $name;
+  public function __construct($name){
+    $this->name = $name;
+  }
+  public function sayHi(){
+    echo "hi! i am $this->name";
+  }
+
+}
